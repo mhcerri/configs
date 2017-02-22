c29c565aa35f69b22e6224592ff96f04135fb24c
Marcelo Cerri <mhcerri@gmail.com>
2017-02-22 14:59:17 -0300

    imapfilter: update sample_config.lua
    Signed-off-by: Marcelo Cerri <mhcerri@gmail.com>

--- home/.imapfilter/sample_config.lua
+++ home/.imapfilter/sample_config.lua
@@ -1,6 +1,9 @@
 -- TODO: copy it as config.lua and edit the configs bellow:
 -- Email account
-pass = io.popen('gpg2 -dq ~/.mutt/my_pass.gpg', 'r'):read('*a')
+status, pass = pipe_from('gpg2 -dq ~/.mutt/my_pass.gpg', 'r')
+if (status ~= 0) then
+	os.exit(1)
+end
 acct = IMAP {
 	server = 'imap.gmail.com',
 	username = '<email>',
@@ -11,12 +14,15 @@ acct = IMAP {
 -- Move important bugs
 lp_user = 'mhcerri'
 bugs = function()
-	return acct['Bugs']:is_recent()
+	return acct.Bugs:is_unseen()
 end
-results = bugs():contain_field('X-Launchpad-Message-For', lp_user)
+results = (
+	bugs():contain_field('X-Launchpad-Message-For', lp_user)
 		:contain_field('X-Launchpad-Message-Rationale', 'Subscriber')
 		:match_field('X-Launchpad-Message-Rationale', '^Subscriber$') +
 	bugs():contain_field('X-Launchpad-Bug-Commenters', lp_user) +
 	bugs():contain_field('X-Launchpad-Bug-Reporter', lp_user) +
 	bugs():contain_field('X-Launchpad-Bug-Modifier', lp_user)
+)
 results:move_messages(acct['Bugs/Important'])
+bugs():mark_seen()
