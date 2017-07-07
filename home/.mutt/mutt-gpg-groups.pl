e21fae9a2f226f460f33acd4773e8ae341e85381
Marcelo Cerri <mhcerri@gmail.com>
2017-07-07 14:35:16 -0300

    mutt-gpg-groups.pl: indent with tabs
    Signed-off-by: Marcelo Cerri <mhcerri@gmail.com>

--- home/.mutt/mutt-gpg-groups.pl
+++ home/.mutt/mutt-gpg-groups.pl
@@ -10,20 +10,20 @@
 use strict;
 open(GROUPS, "gpg --with-colons --list-config group |") or die;
 while (<GROUPS>) {
-    if (/^cfg:group:([^:]*):(.*)/){
-        my $group = $1;
-        my @addrs = split(";", $2);
-        for my $addr (@addrs) {
-            open(KEY, "gpg --list-key \"$addr\" |") or die;
-            while (<KEY>) {
-                if (/^pub\s+.*\/([[:xdigit:]]+)/) {
-                    printf("crypt-hook %s 0x%s # %s\n",
-                        $group, $1, $addr);
-                    last;
-                }
-            }
-            close(KEY);
-        }
-    }
+		if (/^cfg:group:([^:]*):(.*)/){
+		my $group = $1;
+		my @addrs = split(";", $2);
+		for my $addr (@addrs) {
+			open(KEY, "gpg --list-key \"$addr\" |") or die;
+			while (<KEY>) {
+				if (/^pub\s+.*\/([[:xdigit:]]+)/) {
+					printf("crypt-hook %s 0x%s # %s\n",
+					$group, $1, $addr);
+					last;
+				}
+			}
+			close(KEY);
+		}
+	}
 }
 close(GROUPS);
