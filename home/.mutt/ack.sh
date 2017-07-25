53ba7a320044f143614688ebd98abc3d49234f9a
Marcelo Cerri <mhcerri@gmail.com>
2017-07-25 09:34:17 -0300

    mutt: add Acked-by tag when using the ack macro
    Signed-off-by: Marcelo Cerri <mhcerri@gmail.com>

--- home/.mutt/ack.sh
+++ home/.mutt/ack.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+realname=$(mutt -D 2> /dev/null | sed -n -r '/^realname="(.*)"/s//\1/p')
+from=$(mutt -D 2> /dev/null | sed -n -r '/^from="(.*)"/s//\1/p')
 sed -r \
+	-e $'$a\\\nAcked-by: '"$realname"' <'"$from"'>' \
 	-e '0,/^$/!d' \
 	-e 's/(Subject:)\s*([Rr][Ee]:)?(.*)/\1 ACK:\3/' \
 	-i "$*"
