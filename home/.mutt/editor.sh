8e0bba57ec0e429498fd55384891c224c27b585a
Marcelo Cerri <mhcerri@gmail.com>
2018-03-26 14:21:07 -0300

    .mutt/editor.sh: use emacs when available
    Signed-off-by: Marcelo Cerri <mhcerri@gmail.com>

--- home/.mutt/editor.sh
+++ home/.mutt/editor.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-vim				\
-	-c 'silent! /^$'	\
-	-c 'let @/ = ""'	\
-	-c 'set textwidth=72'	\
-	-c 'set wrap'		\
-	-c 'set nocp'		\
-	-c 'set spell'		\
-	"$@"
+if which emacsclient &> /dev/null; then
+	emacsclient -a "" -t "$@"
+else
+	vim				\
+		-c 'silent! /^$'	\
+		-c 'let @/ = ""'	\
+		-c 'set textwidth=72'	\
+		-c 'set wrap'		\
+		-c 'set nocp'		\
+		-c 'set spell'		\
+		"$@"
+fi
