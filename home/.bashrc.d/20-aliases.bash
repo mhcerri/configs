a484bb10e0bf3bb34da292935b48175e3e50ef3c
Marcelo Cerri <mhcerri@gmail.com>
2018-05-09 17:50:10 -0300

    .bashrc: move aliases configuration to .bashrc.d/

---
+++ home/.bashrc.d/20-aliases.bash
@@ -0,0 +1,10 @@
+#
+# Aliases
+#
+
+alias ls='ls --color=auto'
+alias ll='ls -l'
+
+# Add an "alert" alias for long running commands.  Use like so:
+#   sleep 10; alert
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
