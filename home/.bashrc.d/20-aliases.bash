7c253c2f6b17d9082bb0cc47829eceed12b58f92
Marcelo Cerri <mhcerri@gmail.com>
2018-05-09 18:03:09 -0300

    .bashrc.d/20-aliases.bash: add gcd

--- home/.bashrc.d/20-aliases.bash
+++ home/.bashrc.d/20-aliases.bash
@@ -5,6 +5,9 @@
 alias ls='ls --color=auto'
 alias ll='ls -l'
 
+# Jump to the root directory of a git repository
+alias gcd='git rev-parse --show-toplevel 2> /dev/null && cd $(git rev-parse --show-toplevel) || echo "Not a git repo"'
+
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
