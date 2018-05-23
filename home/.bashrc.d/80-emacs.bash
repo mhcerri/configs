2e1684cfe02896fabcfe5a8eca14d3e639baa6e7
Marcelo Cerri <mhcerri@gmail.com>
2018-05-23 11:59:52 -0300

    Fix PATH for custom emacs binaries
    Signed-off-by: Marcelo Cerri <mhcerri@gmail.com>

--- home/.bashrc.d/80-emacs.bash
+++ home/.bashrc.d/80-emacs.bash
@@ -2,12 +2,8 @@
 # Emacs
 #
 
-# Try to use any custom emacs version available at /opt
-_opt_emacs=$(find /opt -mindepth 1 -maxdepth 1 -name 'emacs*' | sort | tail -n1)
-if [[ -n $_opt_emacs ]]; then
-	export PATH="$_opt_emacs/bin/:$PATH"
-fi
-unset _opt_emacs
+# Note: use ~/.profile or a similar file to add a customized emacs to
+#       the PATH.
 
 # Use emacs instead of vim
 if which e &> /dev/null; then
