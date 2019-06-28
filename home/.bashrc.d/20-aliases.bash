3797b9beb31e55665ea70796a598889bc81d41e2
Marcelo Cerri <mhcerri@gmail.com>
2019-06-28 11:22:09 -0300

    Add g alis for git
    Signed-off-by: Marcelo Cerri <mhcerri@gmail.com>

--- home/.bashrc.d/20-aliases.bash
+++ home/.bashrc.d/20-aliases.bash
@@ -1,6 +1,13 @@
 #
 # Aliases
 #
+# Tips:
+#
+# 1. Use "complete -p <cmd>" to get the completion specification of a
+#    command you want to create an alias for.
+# 2. Use "_completion_loader <cmd>" to source the necessary completion
+#    function.
+#
 
 alias ls='ls --color=auto'
 alias ll='ls -l'
@@ -12,3 +19,8 @@ alias gcd='git rev-parse --show-toplevel 2> /dev/null && cd $(git rev-parse --sh
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+
+# Git
+alias g=git
+_completion_loader git
+complete -o bashdefault -o default -o nospace -F __git_wrap__git_main g
