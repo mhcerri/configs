306e7ba6a3de6a1ab74bfae75406085975f91409
Marcelo Cerri <mhcerri@gmail.com>
2019-10-01 16:29:06 -0300

    bashrc: improve OS detection
    Signed-off-by: Marcelo Cerri <mhcerri@gmail.com>

--- home/.bashrc.d/01-os.bash
+++ home/.bashrc.d/01-os.bash
@@ -1,3 +1,8 @@
-_id=$(lsb_release -i |
-	      awk -vFS=':\\s*' '{print $2}' |
-	      tr '[:upper:]' '[:lower:]')
+_id=
+if command -v lsb_release &> /dev/null; then
+	_id=$(lsb_release -i | awk -vFS=':\\s*' '{print $2}')
+elif [ -f /etc/os-release ]; then
+	source /etc/os-release
+	_id="${ID:$ID_LIKE}"
+fi
+_id=$(echo "$_id" | tr '[:upper:]' '[:lower:]')
