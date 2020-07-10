633003cb292764878f9a676bf4d81825b3edc73e
Marcelo Henrique Cerri <mhcerri@gmail.com>
2020-07-10 14:15:25 -0300

    mutt: Update actions.d scripts
    Signed-off-by: Marcelo Henrique Cerri <mhcerri@gmail.com>

--- home/.mutt/action.d/ack.sh
+++ home/.mutt/action.d/ack.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-realname=$(mutt -D 2> /dev/null | sed -n -r '/^realname="(.*)"/s//\1/p')
-from=$(mutt -D 2> /dev/null | sed -n -r '/^from="(.*)"/s//\1/p')
-sed -r \
-	-e $'$a\\\nAcked-by: '"$realname"' <'"$from"'>' \
-	-e '0,/^$/!d' \
-	-e 's/(Subject:)\s*([Rr][Ee]:)?(.*)/\1 ACK:\3/' \
-	-i "$*"
+realname=$(mutt -D 2> /dev/null | sed -n -r '/^(set\s+)?realname\s*=\s*"(.*)"/s//\2/p')
+from=$(mutt -D 2> /dev/null | sed -n -r '/^(set\s+)?from\s*=\s*"(.*)"/s//\2/p')
+awk -vrealname="$realname" -vfrom="$from" '
+	!edited && /^$/ {
+		printf("\nAcked-by: %s <%s>\n", realname, from);
+		edited = 1;
+	}
+	/^Subject:/ {
+		    $0 = gensub(/(Subject:)\s*([Rr][Ee]:)?(.*)/, "\\1 ACK:\\3", 1, $0);
+	}
+	{
+		print;
+	}
+' "${*}" > "${*}.tmp"
+mv "${*}.tmp" "${*}"
--- home/.mutt/action.d/applied.sh
+++ home/.mutt/action.d/applied.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-realname=$(mutt -D 2> /dev/null | sed -n -r '/^realname="(.*)"/s//\1/p')
-from=$(mutt -D 2> /dev/null | sed -n -r '/^from="(.*)"/s//\1/p')
-sed -r \
-	-e '0,/^$/!d' \
-	-e 's/(Subject:)\s*([Rr][Ee]:)?(.*)/\1 APPLIED:\3/' \
-	-i "$*"
+realname=$(mutt -D 2> /dev/null | sed -n -r '/^(set\s+)?realname\s*=\s*"(.*)"/s//\2/p')
+from=$(mutt -D 2> /dev/null | sed -n -r '/^(set\s+)?from\s*=\s*"(.*)"/s//\2/p')
+awk -vrealname="$realname" -vfrom="$from" '
+	!edited && /^$/ {
+		printf("\nApplied. Thanks!\n");
+		edited = 1;
+	}
+	/^Subject:/ {
+		    $0 = gensub(/(Subject:)\s*([Rr][Ee]:)?(.*)/, "\\1 APPLIED:\\3", 1, $0);
+	}
+	{
+		print;
+	}
+' "${*}" > "${*}.tmp"
+mv "${*}.tmp" "${*}"
