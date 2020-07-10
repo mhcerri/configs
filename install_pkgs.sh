caf381c6172b2926cd0e1c6ebd878fa6367a38e6
Marcelo Henrique Cerri <mhcerri@gmail.com>
2020-07-10 14:20:03 -0300

    Keep scripts to install link and packages
    Signed-off-by: Marcelo Henrique Cerri <mhcerri@gmail.com>

---
+++ install_pkgs.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+sudo apt-get install \
+     i3 i3blocks i3lock rofi xss-lock \
+     xfce4 xfce4-power-manager xfce4-notifyd \
+     rxvt-unicode-256color tmux \
+     tlp tlp-rdw cpupower-gui cpufrequtils powertop \
+     emacs vim \
+     x11-xserver-utils xclip xsel \
+     network-manager-gnome blueman \
+     ripgrep fzf fd-find \
+     playerctl \
+     autokey-gtk \
+     cryfs gocryptfs sirikali
+
+# To check:
+# autosuspend
