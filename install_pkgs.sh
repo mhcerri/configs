a2d4a7e3c57e1122023031a53c8b4030628228dd
Marcelo Henrique Cerri <mhcerri@gmail.com>
2020-07-10 14:50:19 -0300

    Update install_pkgs.sh
    Signed-off-by: Marcelo Henrique Cerri <mhcerri@gmail.com>

--- install_pkgs.sh
+++ install_pkgs.sh
@@ -3,7 +3,7 @@ sudo apt-get install \
      i3 i3blocks i3lock rofi xss-lock \
      xfce4 xfce4-power-manager xfce4-notifyd \
      rxvt-unicode-256color tmux \
-     tlp tlp-rdw cpupower-gui cpufrequtils powertop \
+     tlp tlp-rdw cpupower-gui cpufrequtils powertop psensor \
      emacs vim \
      x11-xserver-utils xclip xsel \
      network-manager-gnome blueman \
