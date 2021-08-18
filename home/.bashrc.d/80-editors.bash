c2cb50e3ad11b97be1b5c542dd2af148a388c308
Marcelo Henrique Cerri <mhcerri@gmail.com>
2021-08-18 18:30:06 -0300

    bashrc: Update editors fragment
    Signed-off-by: Marcelo Henrique Cerri <mhcerri@gmail.com>

---
+++ home/.bashrc.d/80-editors.bash
@@ -0,0 +1,15 @@
+#
+# Emacs
+#
+
+# Note: use ~/.profile or a similar file to add a customized emacs to
+#       the PATH.
+
+# Kill the muscle memory
+alias vim='echo -e "\033[1;31mTry \`e\` or \`v\`...\033[0m"'
+
+# Useful aliases
+# not need for "alies e=" because I have a helper script for emacsclient
+# already named "e".
+alias emacs='emacs -nw --no-site-file'
+alias v=lvim
