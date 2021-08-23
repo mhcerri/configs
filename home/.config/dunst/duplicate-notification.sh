34916c43713da1d42df22983601fb554b8ed6145
Marcelo Henrique Cerri <mhcerri@gmail.com>
2021-08-23 17:43:35 -0300

    dunst: Workaround for mattermost
    Duplicate the messages from mattermost as an workaround for the bad
    timeout implementation they have for notifications:
    
    https://github.com/mattermost/mattermost-webapp/blob/master/utils/notifications.tsx#L88
    
    Signed-off-by: Marcelo Henrique Cerri <mhcerri@gmail.com>

---
+++ home/.config/dunst/duplicate-notification.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# ./script appname summary body icon urgency
+appname="$1"
+summary="$2"
+body="$3"
+icon="$4"
+urgency="$5"
+
+tag='[fix]'
+
+if echo "$summary" | grep -F "$tag" &> /dev/null; then
+	echo "Skipping tagged notification."
+	exit 0
+fi
+
+notify-send \
+	-i "$icon" \
+	-u "$urgency" \
+	"$summary $tag" \
+	"$body"
