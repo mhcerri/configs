6d390c2d2258fc2ebb64bd0a45a11e8855211d8d
Marcelo Henrique Cerri <mhcerri@gmail.com>
2022-01-20 12:28:51 -0300

    base: Add time to the prompt
    Signed-off-by: Marcelo Henrique Cerri <mhcerri@gmail.com>

--- home/.bashrc.d/20-prompt.bash
+++ home/.bashrc.d/20-prompt.bash
@@ -49,11 +49,35 @@ window_ps1="$([[ -n "$WINDOW" ]] && echo -n "[w$WINDOW]")"
 pwd_ps1='\w'
 git_ps1='$(__git_ps1 " (%s)" 2> /dev/null)'
 
+function _ps1_timer_stop {
+	local secs=$(( SECONDS - _ps1_last ))
+	unset _ps1_last
+
+	if [ "$secs" -lt 3 ]; then
+		_ps1_timer=
+		return
+	fi
+
+	_ps1_timer='['
+	local h=$(( secs/60/60 ))
+	local m=$(( secs/60%60 ))
+	local s=$(( secs%60 ))
+	(( "$h" > 0 )) && _ps1_timer+="${h}h "
+	(( "$m" > 0 )) && _ps1_timer+="${m}m "
+	_ps1_timer+="${s}s] "
+}
+
+trap '_ps1_last="${_ps1_last:-$SECONDS}"' DEBUG
+PROMPT_COMMAND=_ps1_timer_stop
+
+timer_ps1='${_ps1_timer}'
+
 # Add color to fragments
 if [ "$color_prompt" = yes ]; then
 	# No color for debian_chroot_ps1
 	[ -n "$rc_ps1"     ] &&     rc_ps1="\[\033[01;31m\]$rc_ps1\[\033[01;00m\]"
 	[ -n "$jobs_ps1"   ] &&   jobs_ps1="\[\033[01;32m\]$jobs_ps1\[\033[01;00m\]"
+	[ -n "$timer_ps1"  ] &&  timer_ps1="\[\033[01;90m\]$timer_ps1\[\033[01;00m\]"
 	[ -n "$login_ps1"  ] &&  login_ps1="\[\033[01;38;5;208m\]$login_ps1\[\033[00m\]"
 	[ -n "$window_ps1" ] && window_ps1="\[\033[01;32m\]$window_ps1\[\033[00m\]"
 	[ -n "$pwd_ps1"    ] &&    pwd_ps1="\[\033[01;34m\]$pwd_ps1\[\033[00m\]"
@@ -61,20 +85,19 @@ if [ "$color_prompt" = yes ]; then
 fi
 
 # Set prompt
-export PS1="$debian_chroot_ps1$rc_ps1$jobs_ps1$login_ps1$window_ps1 $pwd_ps1$git_ps1\$ "
+export PS1="$timer_ps1$debian_chroot_ps1$rc_ps1$jobs_ps1$login_ps1$window_ps1 $pwd_ps1$git_ps1\$ "
 export SUDO_PS1="$( echo -n "$PS1" | sed -e 's/;32m/;31m/g' )"
 export ALT_PS1="$PS1\[\a\]"
 unset debian_chroot_ps1 rc_ps1 login_ps1 window_ps1 pwd_ps1 git_ps1 jobs_ps1
-unset color_prompt color_prompt_when_supported
+unset color_prompt color_prompt_when_supported timer_ps1
 
 # Terminal title
 case "$TERM" in
 dumb|eterm*)
 	# Do not set title for emacs terminals
-	export PROMPT_COMMAND=
 	;;
 *)
-	export PROMPT_COMMAND='echo -ne "\033]0;${USER}${HOSTNAME} ${PWD}\007"'
+	export PROMPT_COMMAND='echo -ne "\033]0;${USER}${HOSTNAME} ${PWD}\007"; '"$PROMPT_COMMAND"
 esac
 
 # Bell the terminal when the prompt is shown
