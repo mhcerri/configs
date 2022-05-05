2466c453f0996f634ec427c84fa175d99de7af6e
Marcelo Henrique Cerri <mhcerri@gmail.com>
2022-05-05 06:30:25 -0300

    bashrc: Make OS detection faster
    Signed-off-by: Marcelo Henrique Cerri <mhcerri@gmail.com>

--- home/.bashrc.d/01-os.bash
+++ home/.bashrc.d/01-os.bash
@@ -1,8 +1,10 @@
 _id=
-if command -v lsb_release &> /dev/null; then
-	_id=$(lsb_release -i | awk -vFS=':\\s*' '{print $2}')
-elif [ -f /etc/os-release ]; then
+if [ -f /etc/os-release ]; then
 	source /etc/os-release
 	_id="${ID:$ID_LIKE}"
+elif command -v lsb_release &> /dev/null; then
+	_id=$(lsb_release -i)
+	_id="${_id##*:}"
+	_id="${_id## }"
 fi
 _id=$(echo "$_id" | tr '[:upper:]' '[:lower:]')
